1a775e4e6e659fe26cfffb5e25d69a6676feb5fd
shanyuqin <shanyq@xingyuanauto.com>
2019-07-24 10:40:35 +0800

    Initial Commit

---
+++ TestDecoupleDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDecoupleDemo
+//
+//  Created by shanyuqin on 2019/7/24.
+//  Copyright © 2019 ShanYuQin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
